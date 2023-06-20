b01b83d063a394f1cb50709dab33d88258ef50dc
rajkumarthirumalai <rajkumarrajkumar061@gmail.com>
2023-06-21 00:37:09 +0530

    Added new feature and fixed a bug

--- registration.php
+++ registration.php
@@ -66,8 +66,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <!-- Material Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
     <style>
-
-
         body {
             /* fallback for old browsers */
             background: #a1c4fd;
@@ -90,100 +88,98 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                 border-radius: 15px;
             }
         }
-        label{
+
+        label {
             color: black;
         }
     </style>
 </head>
 
 <body>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-6 ">
+
+        <div class="container py-5">
+
+            <div class="card card-registration card-registration-2 border-0 p-5" style="border-radius: 15px;">
                 <?php if (isset($registrationSuccess) && $registrationSuccess) { ?>
                     <div class="alert alert-success" role="alert">
                         Registration successful!
                     </div>
                 <?php } ?>
                 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-                    <div class="col-lg-12 bg-indigo text-white p-5" style="border-radius:15px">
-                        <div class="mb-3 center" style="text-align:center">
-                            <img src="./Public/log.jpg" alt="Logo" width="90" height="90" style="border-radius:25px; margin-bottom:10px">
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6 mb-4">
-                                <input type="text" class="form-control" id="firstname" name="firstname" required>
-                                <label for="firstname form-label" class="form-label">First Name</label>
-
-                            </div>
-                            <div class="col-md-6 mb-4">
-                                <input type="text" class="form-control" id="lastname" name="lastname" required>
-                                <label for="lastname" class="form-label">Last Name</label>
-
-                            </div>
-                        </div>
-                        <div class="mb-3">
-                            <input type="email" class="form-control" id="emailid" name="emailid" required>
-                            <label for="emailid" class="form-label">Email</label>
-                        </div>
-                        <div class="mb-3">
-                            <input type="password" class="form-control" id="password" name="password" required>
-                            <label for="password" class="form-label">Password</label>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6 mb-4">
-                                <input type="text" class="form-control" id="phone_number" name="phone_number" required>
-                                <label for="phone_number" class="form-label">Phone Number</label>
-                            </div>
-                            <div class="col-md-6 mb-4">
-
-                                <input type="date" class="form-control" id="date_of_birth" name="date_of_birth"
-                                    required>
-                                <label for="date_of_birth" class="form-label">Date of Birth</label>
-                            </div>
-                        </div>
-                        <div class="mb-3">
-                            <input type="text" class="form-control" id="address_lane" name="address_lane" required>
-                            <label for="address_lane" class="form-label">Address Lane</label>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-3 mb-4">
-                                <input type="text" class="form-control" id="city" name="city" required>
-                                <label for="city" class="form-label">City</label>
-                            </div>
-                            <div class="col-md-3 mb-4">
-                                <input type="text" class="form-control" id="state" name="state" required>
-                                <label for="state" class="form-label">State</label>
-                            </div>
-                            <div class="col-md-3 mb-4">
-                                <input type="text" class="form-control" id="country" name="country" required>
-                                <label for="country" class="form-label">Country</label>
-                            </div>
-                            <div class="col-md-3 mb-4">
-                                <input type="text" class="form-control" id="zipcode" name="zipcode" required>
-                                <label for="zipcode" class="form-label">Zip Code</label>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6 mb-4">
-                                <input type="text" class="form-control" id="account_number" name="account_number"
-                                    required>
-                                <label for="account_number" class="form-label">Account Number</label>
-                            </div>
-                            <div class="col-md-6 mb-4">
-                                <input type="number" step="0.01" class="form-control" id="capital_amount"
-                                    name="capital_amount" required>
-                                <label for="capital_amount" class="form-label">Capital Amount</label>
-                            </div>
-                        </div>
-
-                        <button type="submit" class="btn btn-dark btn-lg"
-                            data-mdb-ripple-color="dark">Register</button>
+                    <div class="mb-3 center" style="text-align:center">
+                        <img src="./Public/log.jpg" alt="Logo" width="90" height="90"
+                            style="border-radius:25px; margin-bottom:10px">
                     </div>
+                    <div class="row">
+                        <div class="col-lg-6 col-md-12 mb-4">
+                            <input type="text" class="form-control" id="firstname" name="firstname" required>
+                            <label for="firstname" class="form-label">First Name</label>
+
+                        </div>
+                        <div class="col-lg-6 col-md-12 mb-4">
+                            <input type="text" class="form-control" id="lastname" name="lastname" required>
+                            <label for="lastname" class="form-label">Last Name</label>
+
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <input type="email" class="form-control" id="emailid" name="emailid" required>
+                        <label for="emailid" class="form-label">Email</label>
+                    </div>
+                    <div class="mb-3">
+                        <input type="password" class="form-control" id="password" name="password" required>
+                        <label for="password" class="form-label">Password</label>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6 col-md-12 mb-4">
+                            <input type="text" class="form-control" id="phone_number" name="phone_number" required>
+                            <label for="phone_number" class="form-label">Phone Number</label>
+                        </div>
+                        <div class="col-lg-6 col-md-12 mb-4">
+                            <input type="date" class="form-control" id="date_of_birth" name="date_of_birth" required>
+                            <label for="date_of_birth" class="form-label">Date of Birth</label>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <input type="text" class="form-control" id="address_lane" name="address_lane" required>
+                        <label for="address_lane" class="form-label">Address Lane</label>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-3 col-md-12 mb-4">
+                            <input type="text" class="form-control" id="city" name="city" required>
+                            <label for="city" class="form-label">City</label>
+                        </div>
+                        <div class="col-lg-3 col-md-12 mb-4">
+                            <input type="text" class="form-control" id="state" name="state" required>
+                            <label for="state" class="form-label">State</label>
+                        </div>
+                        <div class="col-lg-3 col-md-12 mb-4">
+                            <input type="text" class="form-control" id="country" name="country" required>
+                            <label for="country" class="form-label">Country</label>
+                        </div>
+                        <div class="col-lg-3 col-md-12 mb-4">
+                            <input type="text" class="form-control" id="zipcode" name="zipcode" required>
+                            <label for="zipcode" class="form-label">Zip Code</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6 col-md-12 mb-4">
+                            <input type="text" class="form-control" id="account_number" name="account_number" required>
+                            <label for="account_number" class="form-label">Account Number</label>
+                        </div>
+                        <div class="col-lg-6 col-md-12 mb-4">
+                            <input type="number" step="0.01" class="form-control" id="capital_amount"
+                                name="capital_amount" required>
+                            <label for="capital_amount" class="form-label">Capital Amount</label>
+                        </div>
+                    </div>
+
+                    <button type="submit" class="btn btn-dark btn-lg" data-mdb-ripple-color="dark">Register</button>
                 </form>
+
             </div>
         </div>
-    </div>
+
 
     <!-- Material Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
@@ -196,8 +192,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             }
         }, 3000);
     </script>
-
-
 
 </body>
 
